2e80109219036e62ada8509a00b1a03c08e9fb0c
Heather Brokmeier <heather@devcollaborative.com>
2022-11-18 10:16:37 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Editoria11y WordPress Plugin
+
+This plugin enqueues the [Editoria11y](https://github.com/itmaybejj/editoria11y) library on the front-end for logged in users.
+
+It uses the default options, and isn't customizable (yet).
+
+* Editoria11y Github: https://github.com/itmaybejj/editoria11y
+* Drupal Module: https://www.drupal.org/project/editoria11y
+* Demo: https://itmaybejj.github.io/editoria11y/demo/)
+
+## How to Install
+
+Download zip and install through the WordPress admin.
---
+++ editoria11y.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: Editoria11y Accessibility Checker
+ * Plugin URI: https://github.com/devcollaborative/editoria11y
+ * Description: A user-friendly accessibility checker, using <a href="https://github.com/itmaybejj/editoria11y">Editoria11y</a>.
+ * Version: 0.1.0
+ * Author: DevCollaborative
+ * Author URI: https://devcollaborative.com/
+ * License:
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Enqueue plugin scripts & styles.
+ */
+function editoria11y_enqueue_scripts() {
+
+	if ( is_user_logged_in() ) {
+		wp_enqueue_script(
+			'editoria11y/js',
+			plugin_dir_url( __FILE__ ) . 'assets/editoria11y.min.js',
+			null,
+			filemtime( plugin_dir_path( __FILE__ ) . 'assets/editoria11y.min.js' ),
+			true,
+		);
+	}
+
+}
+add_action( 'wp_enqueue_scripts', 'editoria11y_enqueue_scripts' );
+
+/**
+ * Initialize Editoria11y.
+ */
+function editoria11y_init(){
+	?>
+		<script>
+			document.addEventListener('DOMContentLoaded', function() {
+				const ed11y = new Ed11y();
+			});
+		</script>
+	<?php
+};
+add_action ('wp_footer', 'editoria11y_init', 100 );
